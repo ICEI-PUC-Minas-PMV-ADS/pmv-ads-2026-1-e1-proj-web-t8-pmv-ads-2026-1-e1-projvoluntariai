f5025b99fb9b5d5c58b455fccf12893244662393
Amanda Soares <amandaluizasoaares@gmail.com>
2026-05-29 20:22:32 -0300

    Criação do fluxo de "Quem Somos"

---
+++ codigo-fonte/tela-QuemSomos/tela-quemsomos.js
@@ -0,0 +1,11 @@
+const botaoVagas = document.querySelector(".btn-vagas");
+
+botaoVagas.addEventListener("click", () => {
+
+    alert("Redirecionando para as vagas disponíveis!");
+
+    // Altere para a página correta do projeto
+    window.location.href = "../TelaVagasVoluntario/Vagas.html";
+
+
+});
